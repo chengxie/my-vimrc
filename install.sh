58c50f8e71175e81cfc4e195db25211b4f3491f4
chengxie <chengxie@me.com>
2019-11-06 23:15:49 +0800

    create install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -fLo ~/.vimrc \
+    https://raw.githubusercontent.com/chengxie/my-vimrc/master/vimrc
+
+echo "We done !"
