9b186755d0745467d193bae373a80f5fbf98a1a3
chengxie <chengxie@me.com>
2019-11-07 14:35:15 +0800

    update

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-echo "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-echo "curl -fLo ~/.vim/coc-settings.json --create-dirs \\
-    https://raw.githubusercontent.com/chengxie/my-vimrc/master/vim/coc-settings.json"
-curl -fLo ~/.vim/coc-settings.json \
-    https://raw.githubusercontent.com/chengxie/my-vimrc/master/vim/coc-settings.json
-
 echo "curl -fLo ~/.vimrc \\
     https://raw.githubusercontent.com/chengxie/my-vimrc/master/vimrc"
 curl -fLo ~/.vimrc \
     https://raw.githubusercontent.com/chengxie/my-vimrc/master/vimrc
 
+echo "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+if [ !  -f "~/.vim/coc-settings.json" ]; then
+    echo "curl -fLo ~/.vim/coc-settings.json --create-dirs \\
+        https://raw.githubusercontent.com/chengxie/my-vimrc/master/vim/coc-settings.json"
+    curl -fLo ~/.vim/coc-settings.json \
+        https://raw.githubusercontent.com/chengxie/my-vimrc/master/vim/coc-settings.json
+fi
 
 vim -c "PlugInstall" < /dev/tty
