e2a9723db607a63a74f86864676d11ba66f66345
chengxie <chengxie@me.com>
2019-11-07 11:26:03 +0800

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+echo "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "curl -fLo ~/.vimrc \\
+    https://raw.githubusercontent.com/chengxie/my-vimrc/master/vimrc"
 curl -fLo ~/.vimrc \
     https://raw.githubusercontent.com/chengxie/my-vimrc/master/vimrc
 
-vim -c "PlugInstall"
+vim -c "PlugInstall" < /dev/tty
