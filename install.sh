39e016c027adc55b4e8a4cde6d790d0d7140ea32
chengxie <chengxie@me.com>
2019-11-07 13:06:58 +0800

    update

--- install.sh
+++ install.sh
@@ -5,9 +5,15 @@ echo "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "curl -fLo ~/.vim/coc-settings.json --create-dirs \\
+    https://raw.githubusercontent.com/chengxie/my-vimrc/master/vim/coc-settings.json"
+curl -fLo ~/.vim/coc-settings.json \
+    https://raw.githubusercontent.com/chengxie/my-vimrc/master/vim/coc-settings.json
+
 echo "curl -fLo ~/.vimrc \\
     https://raw.githubusercontent.com/chengxie/my-vimrc/master/vimrc"
 curl -fLo ~/.vimrc \
     https://raw.githubusercontent.com/chengxie/my-vimrc/master/vimrc
+
 
 vim -c "PlugInstall" < /dev/tty
